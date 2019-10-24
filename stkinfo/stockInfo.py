40596675fdb52f1086595a618ea410a08c8163fc
wlntuer <wlntuer@outlook.com>
2019-10-24 15:29:18 +0800

    enter

---
+++ stkinfo/stockInfo.py
@@ -0,0 +1,56 @@
+import csv
+import urllib.request as r
+import threading
+import clsDir
+ 
+# 读取之前获取的个股csv丢入到一个列表中
+def getStockList():
+    stockList = []
+    f = open('D:/Code_hello/helloworld/stkinfo/stock.csv', 'r', encoding='utf-8')
+    f.seek(0)
+    reader = csv.reader(f)
+ 
+    for item in reader:
+        stockList.append(item)
+    f.close()
+    return stockList
+ 
+ 
+def downloadFile(url, filepath):
+    try:
+        r.urlretrieve(url, filepath)
+    except Exception as e:
+        print(e)
+    print(filepath, "is downloaded")
+    pass
+ 
+ 
+# 设置信号量，控制线程并发数
+sem = threading.Semaphore(100)
+ 
+ 
+def downloadFileSem(url, filepath):
+    with sem:
+        downloadFile(url, filepath)
+ 
+ 
+urlStart = 'http://quotes.money.163.com/service/chddata.html?code='
+urlEnd = '&end=20190701&fields=TCLOSE;HIGH;LOW;TOPEN;LCLOSE;CHG;PCHG;VOTURNOVER;VATURNOVER'
+ 
+if __name__ == '__main__':
+    # 创建目录
+    mkpath = "D:\\Code_hello\helloworld\\StocksInfo"
+    # 调用函数
+    clsDir.mkdir(mkpath)
+ 
+    stockList = getStockList()
+    stockList.pop(0)
+    for s in stockList:
+        scode = str(s[1])
+        # 0：沪市；1：深市
+        url = urlStart + ("0" if scode.startswith('6') else "1") + scode + urlEnd
+        filepath = 'D:/Code_hello/helloworld/StocksInfo/' + (scode + '_' + str(s[0])) + '.csv'
+        threading.Thread(target=downloadFileSem, args=(url, filepath)).start()
+'''————————————————
+版权声明：本文为CSDN博主「Royi666」的原创文章，遵循 CC 4.0 BY-SA 版权协议，转载请附上原文出处链接及本声明。
+原文链接：https://blog.csdn.net/sunroyi666/article/details/95176075'''

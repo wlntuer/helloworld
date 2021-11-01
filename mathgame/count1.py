e27f26cb103298bdce7d4648d6a1563efa321334
wlntuer <wlntuer@outlook.com>
2021-11-01 09:47:20 +0800

    enter

--- mathgame/count1.py
+++ mathgame/count1.py
@@ -9,8 +9,8 @@ while (1):
         if (a % 2 == 1):
             i = i+1
         a = math.floor(a/2)
-        if (a <= 0):
-            break
+        if (a <= 0):1
+           break
     print('%d 的2进制中1的个数是 %d' % (b, i))  # 打印带参数的格式1
     if (b == 0):
         break
